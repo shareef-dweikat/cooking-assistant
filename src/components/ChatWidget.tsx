730e8bc52b391b4fb284bdc777b05d802ae29dee
Shareef Dweikat <sdweikat@asaltech.com>
2023-05-07 10:47:43 +0300

    Add messaging functionality to chatwidget

--- src/components/ChatWidget.tsx
+++ src/components/ChatWidget.tsx
@@ -2,13 +2,42 @@ import styles from '../styles/ChatWidget.module.css'
 import SendIcon from '@mui/icons-material/Send';
 import { CHAT_TITLE, CHAT_WELCOME_MESSAGE, IN_ACTIVE_CHAT_SUB_TITLE, IN_ACTIVE_CHAT_TITLE } from '../constants/strings';
 import { useState } from 'react';
+import { sendMessage } from '../network/controllers/aiAssistant'
+import { Choice } from '../../types'
 
-export default function ChatWidget() {
+interface props {
+  onChange: Function
+}
+
+export default function ChatWidget({ onChange }: props) {
   const [active, setActive] = useState(false)
+  const [messages, setMessages] = useState<Choice[]>([])
+  const [userMessage, setUserMessage] = useState('')
+  const [isWriting, setIswriting] = useState(false)
+
+  const handleSend = async () => {
+    const message = userMessage
+    const messageTemplate = `What are the ingredients of ${message}. Please make it in a JS string seperated by commas`
+
+    setUserMessage('')
+    setMessages((choices: Choice[]) => [...choices, { message: { content: message, role: 'user' } }])
+    setIswriting(true)
+
+    const data = await sendMessage(messageTemplate)
+
+    const choices = data.getChoices()
+    const createdAt = data.getCreatedAt()
+
+    setIswriting(false)
+    setMessages((oldChoices: Choice[]) => [...oldChoices, ...choices])
+
+    onChange(choices[0]?.message.content)
+  }
+
   return (
     active ?
-      <div id={styles.container} onClick={()=> setActive(false)}>
-        <div id={styles.header}>
+      <div id={styles.container}>
+        <div id={styles.header} onClick={() => setActive(false)}>
           <div>
             <div style={{ fontSize: 22 }}>{CHAT_TITLE}</div>
             <div style={{ fontSize: 12, textAlign: 'center' }}>Type your dish name</div>
@@ -18,26 +47,29 @@ export default function ChatWidget() {
           <div id={styles.dialogBox}>
             {CHAT_WELCOME_MESSAGE}
           </div>
-
-          <div id={styles.leftDialogBoxContainer}>
-            <div id={styles.dialogBox}>
-              Pizza
-            </div>
-          </div>
-
-          <div id={styles.dialogBox}>
-            -Cheese
-            -Tomato
-            -Cheese
-            -Peper
-          </div>
+          {messages?.map((item: Choice, index: number) => {
+            if (item.message.role === "assistant") {
+              return <div id={styles.dialogBox} key={index}>{item.message.content}</div>
+            } else {
+              return <div id={styles.leftDialogBoxContainer} key={index}>
+                <div id={styles.dialogBox}>
+                  {item.message.content}
+                </div>
+              </div>
+            }
+          })}
         </div>
+        {isWriting && <div id={styles.AIIsWriting}>AI assistant is writing.....</div>}
         <div id={styles.messageBox}>
-          <input placeholder='Type.....' />
-          <SendIcon />
+          <input placeholder='Type only name of the dish'
+            onChange={(input) => setUserMessage(input.target.value)}
+            value={userMessage}
+          />
+          <div onClick={handleSend}> <SendIcon />
+          </div>
         </div>
       </div>
-      : <div id={styles.inActiveChatcontainer} onClick={()=> setActive(true)}>
+      : <div id={styles.inActiveChatcontainer} onClick={() => setActive(true)}>
         <div>
           <div id={styles.inActiveChatTitle}>{IN_ACTIVE_CHAT_TITLE}</div>
           <div id={styles.inActiveChatSubTitle}>{IN_ACTIVE_CHAT_SUB_TITLE}</div>
