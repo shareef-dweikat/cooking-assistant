fb8a5bbb9969581f6bc394e51c232e8564b590c7
Shareef Dweikat <sdweikat@asaltech.com>
2023-05-15 09:46:59 +0300

    Wire up assistant endpoint

--- src/network/controllers/aiAssistant.ts
+++ src/network/controllers/aiAssistant.ts
@@ -3,7 +3,7 @@ import { TEXT_COMPLETION_ENDPOINT } from '../endpoints'
 import axios from 'axios';
 
 export const sendMessage = async (userMessage: string) => {
-    const API_KEY = ""
+    const API_KEY = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NWEyMGYzZWZjYTc2ODYxYjhlMjYzNCIsImlhdCI6MTY4NDEzMDk4MCwiZXhwIjoxNjg2NzIyOTgwfQ.IIYCNmDputBJMjUkDY4F5OphZFC1ZPrqCjCEEUi_1S4'
     const config = {
         headers: {
             "Authorization": API_KEY,
@@ -11,11 +11,8 @@ export const sendMessage = async (userMessage: string) => {
         }
     };
 
-    const data = {
-        model: "gpt-3.5-turbo",
-        messages: [{ "role": "user", "content": userMessage}]
-    }
+    const data = { }
 
-    let response = await axios.post(TEXT_COMPLETION_ENDPOINT, data, config)
+    let response = await axios.post(`${TEXT_COMPLETION_ENDPOINT}${userMessage}`, data, config)
     return new AIAnswer(response.data.created, response.data.choices)
 }
--- src/network/endpoints.js
+++ src/network/endpoints.js
@@ -1,4 +1,4 @@
 import configs from '../../tsconfig.json'
 
 export const PRODUCTS_ENDPOINT =  `${configs.compilerOptions.baseUrl}/api/v1/products/`
-export const TEXT_COMPLETION_ENDPOINT =  `https://api.openai.com/v1/chat/completions`
+export const TEXT_COMPLETION_ENDPOINT =  `http://localhost:5000/api/v1/assistant/?dish=`
