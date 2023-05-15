22932e73a81d26866d5fee123cc243c62f21f0fb
Shareef Dweikat <sdweikat@asaltech.com>
2023-05-15 13:54:29 +0300

    Enhance chat box UI

--- src/components/ChatWidget.tsx
+++ src/components/ChatWidget.tsx
@@ -1,7 +1,8 @@
 import styles from '../styles/ChatWidget.module.css'
 import SendIcon from '@mui/icons-material/Send';
+import MessageIcon from '@mui/icons-material/Message';
 import { CHAT_TITLE, CHAT_WELCOME_MESSAGE, IN_ACTIVE_CHAT_SUB_TITLE, IN_ACTIVE_CHAT_TITLE } from '../constants/strings';
-import { useState } from 'react';
+import { KeyboardEventHandler, useState } from 'react';
 import { sendMessage } from '../network/controllers/aiAssistant'
 import { Choice } from '../../types'
 
@@ -16,14 +17,12 @@ export default function ChatWidget({ onChange }: props) {
   const [isWriting, setIswriting] = useState(false)
 
   const handleSend = async () => {
-    const message = userMessage
-    const messageTemplate = `What are the ingredients of ${message}. Please make it in a JS string seperated by commas`
 
     setUserMessage('')
-    setMessages((choices: Choice[]) => [...choices, { message: { content: message, role: 'user' } }])
+    setMessages((choices: Choice[]) => [...choices, { message: { content: userMessage, role: 'user' } }])
     setIswriting(true)
 
-    const data = await sendMessage(messageTemplate)
+    const data = await sendMessage(userMessage)
 
     const choices = data.getChoices()
     const createdAt = data.getCreatedAt()
@@ -32,6 +31,12 @@ export default function ChatWidget({ onChange }: props) {
     setMessages((oldChoices: Choice[]) => [...oldChoices, ...choices])
 
     onChange(choices[0]?.message.content)
+  }
+
+  const handleKeyPress = (event: { key: string }) => {
+    if (event.key === 'Enter') {
+      handleSend()
+    }
   }
 
   return (
@@ -58,21 +63,22 @@ export default function ChatWidget({ onChange }: props) {
               </div>
             }
           })}
+          {isWriting && <div id={styles.AIIsWriting}>AI assistant is writing.....</div>}
         </div>
-        {isWriting && <div id={styles.AIIsWriting}>AI assistant is writing.....</div>}
         <div id={styles.messageBox}>
           <input placeholder='Type only name of the dish'
             onChange={(input) => setUserMessage(input.target.value)}
             value={userMessage}
+            onKeyUp={handleKeyPress}
           />
           <div onClick={handleSend}> <SendIcon />
           </div>
         </div>
       </div>
       : <div id={styles.inActiveChatcontainer} onClick={() => setActive(true)}>
-        <div>
-          <div id={styles.inActiveChatTitle}>{IN_ACTIVE_CHAT_TITLE}</div>
-          <div id={styles.inActiveChatSubTitle}>{IN_ACTIVE_CHAT_SUB_TITLE}</div>
+        <div id={styles.inActiveChatHint}>Need help finding the ingredients? <div id={styles.hintArrow}/></div>
+        <div  id={styles.inActiveChatcontainerButton} >
+          <MessageIcon />
         </div>
       </div>
   )
